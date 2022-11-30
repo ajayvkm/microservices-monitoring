574bf0ecace4cc6d997fee8a92fc855d65f288f6
Ajay Vishwakarma <avishwakarma1@illumina.com>
2022-11-30 01:42:32 -0800

    Added Cloud config server for centralized config

--- account-service/src/main/java/com/sdsu/controller/AccountController.java
+++ account-service/src/main/java/com/sdsu/controller/AccountController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/api/v1/accounts")
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:8762")
 public class AccountController {
 
     private final AccountRepository repository;
