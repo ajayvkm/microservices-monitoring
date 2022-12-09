5cb0d7963b37d869b5a525c6b050cb8c4efce407
Ajay Vishwakarma <avishwakarma1@illumina.com>
2022-12-09 02:11:48 -0800

    Fix Json encoder issues for Kibana

---
+++ payment-service/src/main/java/com/sdsu/OpenApiConfiguration.java
@@ -0,0 +1,35 @@
+package com.sdsu;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition
+@AllArgsConstructor
+public class OpenApiConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(getInfo());
+    }
+
+    private Info getInfo() {
+        return new Info()
+                .title("Payment Service")
+                .description("San Diego State University academic project - Payment Service")
+                .version("v1.0.0")
+                .license(getLicense());
+    }
+
+    private License getLicense() {
+        return new License()
+                .name("Ajay Vishwakarma")
+                .url("https://sdsu.edu/");
+    }
+}
